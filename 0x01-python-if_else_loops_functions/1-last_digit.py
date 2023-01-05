4e13af8c70769fac2a489e90d0fddc6827bad51f
Sn3pcity <sn3pcity@gmail.com>
2023-01-04 16:51:13 -0800

    Last Digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+LD = number % 10
+if LD > 5:
+    string = "and is greater than 5"
+    if LD == 0:
+        string = "and is 0"
+        if LD < 6 and LD != 0:
+            string = " and is less than 6 and not 0"
+            if number < 0:
+                LD = -(LD)
+        print("Last digit of {:d} is {:d}".format(number, LD), string)
