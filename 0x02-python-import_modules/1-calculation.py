d2137d94b38b5151856f258e50f502b9265a65d4
Sn3pcity <sn3pcity@gmail.com>
2023-01-06 18:10:57 -0800

    TASK 01

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import *
+if  __name__ == "__main__":
+    a = 10
+    b = 5
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
