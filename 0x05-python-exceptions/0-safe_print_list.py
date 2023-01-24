c26ab10749970d145f73c4f479c5efb6355bd08f
Sn3pcity <sn3pcity@gmail.com>
2023-01-23 18:52:34 -0800

    Indent error change

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
 
-     printed = 0
+    printed = 0
+     
     for i in range(x):
+         
         try:
-            print(f"{my_list[i]:d}".end="")
+            print("{:d}".format(my_list[i]), end="")
             printed += 1
         except:
             continue
