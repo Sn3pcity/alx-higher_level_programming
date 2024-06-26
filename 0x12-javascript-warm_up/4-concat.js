b2103e18e19b5e3613009be6660ec02e2db2f7e5
Sn3pcity <sn3pcity@gmail.com>
2024-06-26 23:18:27 +0300

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// Get the command line arguments
+const args = process.argv.slice(2);
+
+// Check if exactly two arguments are passed
+if (args.length === 2)
+{
+    // Print the arguments in the specified format
+    console.log(`${args[0]} is ${args[1]}`);
+}
